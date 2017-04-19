baa291a8094abb005413b927461cd765d6eb060e
Winston Kotzan <wak@wakproductions.com>
2017-04-18 20:48:47 -0400

    Add modules for testing, API call, and add to docs

--- lib/omega_client/fetch_monthly_prices.ex
+++ lib/omega_client/fetch_monthly_prices.ex
@@ -7,6 +7,10 @@ defmodule OmegaClient.FetchMonthlyPrices do
     
   end
 
+  def api_endpoint do
+    @api_endpoint
+  end
+
   defp params do
     %{
       yourapi_key:  "abc123key",  # TODO put this in a constants module
